d772682786b89d6b7f89f4db410dcc58e2898635
Nope01 <notadam18@gmail.com>
2025-05-03 17:47:59 +1000

    Added folder for documentation files

---
+++ src/main/java/org/lwjgl/objects/entities/Creature.java
@@ -0,0 +1,9 @@
+package org.lwjgl.objects.entities;
+
+import org.lwjgl.objects.SceneObject;
+
+public abstract class Creature extends SceneObject {
+    private String name;
+    private int type;
+    
+}
