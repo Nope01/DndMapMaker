b6166bbe4605c2c2e89b5137fe11c3ae36e4c80b
Horizon64 <mmmyesgames@gmail.com>
2025-05-15 17:16:00 +1000

    NPC Class

---
+++ src/main/java/org/lwjgl/objects/entities/NPCs.java
@@ -0,0 +1,25 @@
+package org.lwjgl.objects.entities;
+
+public final class NPCs {
+
+    public static final int ASSASSIN = 0;
+    public static final int BEHOLDER = 1;
+    public static final int COMMONER = 2;
+    public static final int  = 3;
+    public static final int  = 4;
+    public static final int  = 5;
+    public static final int  = 6;
+    public static final int  = 7;
+    public static final int  = 8;
+    public static final int  = 9;
+    public static final int  = 10;
+    public static final int  = 11;
+    public static final int  = 12;
+
+    public static final String[] npcList = new String[] {
+            "Assassin",
+            "Beholder",
+            "Commoner",
+            "",
+    };
+}
